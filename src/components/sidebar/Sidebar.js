324e503f129fd2740b06fdc839062f5abfee3ee9
0611NASKA <010611naoki@gmail.com>
2023-05-21 04:17:06 +0900

    [Add]Twitterアイコンを追加

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-      
+      <TwitterIcon className="sidebar_twitterIcon" />
     </div>
   )
 }
