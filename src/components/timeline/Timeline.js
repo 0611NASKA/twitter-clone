5fd1d250d041a6539023bc8e41bdbadc79b1e234
0611NASKA <010611naoki@gmail.com>
2023-05-21 18:37:53 +0900

    タイムラインcss作成

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import "./Timeline.css";
 
 function Timeline() {
   return (
     <div className='timeline'>
-      <div className='timeline--header'>
+      <div className='timeline_header'>
         <h2>ホーム</h2>
       </div>
     </div>
