725c95aacf2dead3fae3dbaf98fb95adc8e37b22
0611NASKA <010611naoki@gmail.com>
2023-05-21 18:31:10 +0900

    [Start]タイムライン作成

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+      <div className='timeline--header'>
+        <h2>ホーム</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Timeline
