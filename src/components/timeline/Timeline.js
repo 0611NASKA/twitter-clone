f2e1641b815ecf93469fbbfab4eb92d64c310174
0611NASKA <010611naoki@gmail.com>
2023-05-21 19:41:08 +0900

    post.js作成

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Timeline.css";
 import TweetBox from './TweetBox';
+import Post from './Post';
 
 function Timeline() {
   return (
@@ -10,6 +11,8 @@ function Timeline() {
       </div>
 
       <TweetBox />
+
+      <Post />
     </div>
   )
 }
