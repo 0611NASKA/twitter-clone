b3ec14fbf178a1eff825a7f8837a2f6743a7ed86
0611NASKA <010611naoki@gmail.com>
2023-05-21 18:58:47 +0900

    TweetBoxのcssを作成

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,13 +1,16 @@
-import { Avatar, Button } from '@mui/material'
-import React from 'react'
+import { Avatar, Button } from '@mui/material';
+import React from 'react';
+import "./TweetBox.css";
 
 function TweetBox() {
   return (
     <div className='tweetBox'>
       <form>
-        <Avatar />
-        <input placeholder='いまどうしてる？' type='text'></input>
-        <input className='tweetBox_ImageInput' placeholder='画像のURLを入力してください' type='text'></input>
+        <div className='tweetBox_input'>
+          <Avatar />
+          <input placeholder='いまどうしてる？' type='text'></input>
+        </div>
+        <input className='tweetBox_Imageinput' placeholder='画像のURLを入力してください' type='text'></input>
         <Button className='tweetBox_tweetButton' type="submit">ツイートする</Button>
       </form>
     </div>
